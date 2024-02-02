dd46a21f4ac0a41d10b9fbde27ecc29167eb093f
ryzenforce990 <ryzenforce990@gmail.com>
2024-02-02 04:06:13 +0000

    Revert "vendor: Add Black theme [3/3] (#1)"
    This reverts commit b8e0803005d366bfd02f2b5ad307efbc16c6ccf7.

--- config/common.mk
+++ config/common.mk
@@ -128,10 +128,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true \
 
-#Black Theme
-PRODUCT_PACKAGES += \
-    AndroidBlackThemeOverlay
-
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUIGoogle \
